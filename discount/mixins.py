7d29b76ce7f3c33bd9b9ff37a83ce3b297ec9e4f
Rocky Meza <rocky@fusionbox.com>
2012-12-06 13:35:00 -0700

    negative discount

--- discount/mixins.py
+++ discount/mixins.py
@@ -12,7 +12,7 @@ class PercentDiscountMixin(models.Model):
 
     def get_extra_cart_price_field(self, cart):
         amount = ((self.amount / 100) * cart.subtotal_price).quantize(Decimal('1.00'))
-        return (self.get_name(), amount,)
+        return (self.get_name(), -amount,)
 
     class Meta:
         abstract = True
